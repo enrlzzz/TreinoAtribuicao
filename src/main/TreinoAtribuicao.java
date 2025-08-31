26794a8622e146fb8a1d4823e54a7914df3e3a72
enrlzzz <enricolg16@gmail.com>
2025-08-31 12:12:12 -0300

    FEAT: Primeiro commit

---
+++ src/main/TreinoAtribuicao.java
@@ -0,0 +1,19 @@
+package main;
+
+
+public class TreinoAtribuicao {
+    public static void main(String[] args) {
+        
+//        VARIAVEL DE TIPO INTEIRO, RESULTA EM VALOR INTEIRO, POR ISSO 2 E NÃO 2.5
+        int a, b;
+        double resultado;
+        
+        a = 5;
+        b = 2;
+        
+        resultado = a / b;
+        
+        System.out.println(resultado); 
+        
+    }
+}
